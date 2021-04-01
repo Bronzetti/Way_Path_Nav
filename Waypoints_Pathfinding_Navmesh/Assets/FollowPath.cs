022b3a2e7ea3ca39cf716be52bc3ebcedd7f4c04
Bronzetti <68982370+Bronzetti@users.noreply.github.com>
2021-03-31 21:40:19 -0300

    Update

---
+++ Waypoints_Pathfinding_Navmesh/Assets/FollowPath.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour //movimentação do tank
+{
+    Transform goal;//pontos que o objeto irá
+    float speed= 5.0f;//velocidade do tank
+    float accuracy= 1.0f;//aproximação do objeto com o ponto pra poder já rotacionar para o próximo
+    float rotSpeed= 2.0f;//velocidade de rotação do objeto
+    public GameObject wpManager;//ciração do gameobject wpManager 
+    GameObject[] wps; // array com index wps
+    GameObject currentNode;//gameobject de cada nó
+    int currentWP= 0; //necessita ser igual a zero inicialmente para iniciar a movimentação
+    Graph g;//refenencia meu arquivo Graph 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;//gerenciamento dos componentes dentro do WPManager 
+        g = wpManager.GetComponent<WPManager>().graph; //cada waypoint possui uma informação desse gerenciamento dentro do array
+        currentNode = wps[0];//matriz abastecida com cada ponto criado no mapa para que seja feito a trajetória correta
+
+    }
+    public void GoToHeli()//Método 
+    {
+        g.AStar(currentNode, wps[1]);// g.ASrtar e local aonde tá criando meu mapa
+        currentWP = 0; //ponto final no qual estará percorrendo 
+    }
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[6]); 
+        currentWP = 0; 
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())//condicional que pega o tamanho certo do path que o objeto caminhará
+            return;
+        //O nó que estará mais próximo neste momentocurrentNode= g.getPathPoint(currentWP);
+        //se estivermos mais próximo bastante do nó o tanque se moverá para o próximoif
+        if(Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+            {
+            currentWP++;
+            }
+
+        if (currentWP < g.getPathLength()) //currentWP com tamanho inferior ao getPath
+        { 
+            goal = g.getPathPoint(currentWP).transform;// verifica o atual e já calcula pro próximo
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);//movimentação mirando pro próximo ponto com rotação
+            Vector3 direction = lookAtGoal - this.transform.position; 
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 
+                Time.deltaTime * rotSpeed); //Slerp com movimentação mais suave
+        }
+    }
+}
---
+++ Waypoints_Pathfinding_Navmesh/Assets/WPManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] //tranfere os dados para diferentes classes de forma que eles se entendam
+public struct Link //struct para construção das conexões de um ponto ao outro
+{
+    public enum direction { UNI, BI } //variável de direção com sentidos e direções para grafos. Sendo UNI um único sentido e BI, ambos sentidos
+    public GameObject node1; //ponto inicial
+    public GameObject node2; //segundo ponto 
+    public direction dir; //variável de direção
+}
+public class WPManager : MonoBehaviour
+{
+    public GameObject[] waypoints; //array
+    public Link[] links; //array de informação do link do struct criado anteriormente 
+    public Graph graph = new Graph(); //pega as informações do arquivo Graph
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (waypoints.Length > 0) //se o tamanho do meu array for diferente, ou seja, maior que zero
+        { 
+            foreach (GameObject wp in waypoints)//completa o array criado
+            { 
+                graph.AddNode(wp); //abastecimento do array, ponto no mapa 
+            } 
+            foreach (Link l in links) //cada ponto colocado no mapa, é possível adicionar uma linha ligando um ponto ao outro
+            { 
+                graph.AddEdge(l.node1, l.node2); //adição da aresta entre dois pontos
+                if (l.dir == Link.direction.BI)// condição se a aresta será ida e volta 
+                    graph.AddEdge(l.node2, l.node1); //ponto 2 volta para ponto 1, trajeto da volta 
+            } 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        graph.debugDraw(); //Desenha a aresta no cena
+    }
+}
